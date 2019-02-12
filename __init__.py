08506e9865139a0007f7a02666bb8ad14ee611a9
Riccardo Fogliato <rfogliat@andrew.cmu.edu>
2019-02-12 13:05:02 -0500

    init file

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .get_browser import *
+from .login import *
+from .get_runner import *
+from .scrape_history import *
+
+# alternative call for wild import
+__all__ = ['get_browser', 'login', 'get_runner', 'scrape_history']
