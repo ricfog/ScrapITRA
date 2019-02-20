c57cf4654f6ae2f1ccdd7fa24322cec8ba67f36e
Riccardo Fogliato <rfogliat@andrew.cmu.edu>
2019-02-19 22:07:42 -0500

    adapted to firefox

--- get_races.py
+++ get_races.py
@@ -7,7 +7,7 @@ scrape races
 from ScrapITRA.import_modules import *
 
 
-def scrape_races(browser, start_date, end_date):
+def get_races(browser, start_date, end_date):
 
     browser.get('https://itra.run/page/328/Results.html')
     WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.ID, 'cal')))
@@ -22,14 +22,16 @@ def scrape_races(browser, start_date, end_date):
     start = browser.find_element_by_name("dtmin")
     start.clear()
     start.send_keys(start_date)
+    start.send_keys(u'\ue007')
+
+    time.sleep(4)
 
     end = browser.find_element_by_name("dtmax")
     end.clear()
     end.send_keys(end_date)
+    end.send_keys(u'\ue007')
 
-
-
-    time.sleep(10)
+    time.sleep(4)
 
     store_race = []
 
@@ -41,18 +43,20 @@ def scrape_races(browser, start_date, end_date):
         store_page = []
         race_table = browser.find_element_by_id('cal')
 
-        n_el = len(race_table.find_elements_by_css_selector("[onclick*='getResult']"))
+        results = race_table.find_elements_by_css_selector("[onclick*='getResult']")
+        time.sleep(4)
 
+        race_info = race_table.find_elements_by_class_name('race')
 
-        for it in range(n_el):
-
+        for it in range(len(results)):
 
             # click on the race
-            info = race_table.find_elements_by_class_name('race')[it].text.split('\n')
+            info = race_info[it].text.split('\n')
             info[1] = info[1].replace(',','')
             store_page.append(info[:5])
-            race = race_table.find_elements_by_css_selector("[onclick*='getResult']")[it]
-            race.send_keys("\n")
+
+            race = results[it]
+            race.click()
 
             WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, 'result')))
 
@@ -66,14 +70,14 @@ def scrape_races(browser, start_date, end_date):
                 store_page.append(row)
 
             browser.find_element_by_xpath('/html/body/div[4]/div[1]/a/span').click()
-            browser.find_element_by_id('fdroite').click()
 
 
-
-        race_store.append(store_page)
+        browser.find_element_by_id('fdroite').click()
+        store_race.append(store_page)
 
         time.sleep(10)
         max_page = int(float(browser.find_element_by_id('nbpmax').text))
         page_n += 1
 
+    browser.quit()
     return store_race
