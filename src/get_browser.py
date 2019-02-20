51efeae2938b8cefdfc52d0951a0d4d3a22b4f78
Riccardo Fogliato <rfogliat@andrew.cmu.edu>
2019-02-20 15:51:32 -0500

    do not remember what to commit but need to do it

--- src/get_browser.py
+++ src/get_browser.py
@@ -13,11 +13,13 @@ import os
 
 
 
-def get_browser(webpage, head=True):
+def get_browser(webpage, head=True, path_driver=None):
     options = Options()
     options.headless = head # go headless
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    path_driver = os.path.join(dir_path,'Drivers/firefox_mac')
+    if not path_driver:
+        path_driver = 'Drivers/firefox_mac'
+    #dir_path = os.path.dirname(os.path.realpath(__file__))
+    #path_driver = os.path.join(dir_path,'Drivers/firefox_mac')
     browser = webdriver.Firefox(options=options, executable_path=path_driver)
 
     browser.get(webpage)
