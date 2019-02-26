9f104f2eeadad28689f8c41ceed0210e2cf72fd4
Riccardo Fogliato <rfogliat@andrew.cmu.edu>
2019-02-26 10:00:14 -0800

    transformed into package

--- bin/write_races.py
+++ bin/write_races.py
@@ -1,4 +1,4 @@
-from ..src import *
+from ..scrapITRA.src import *
 
 import argparse
 
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='ScrapITRA',
+    version='0.1.0',
+    author='Riccardo Fogliato',
+    author_email='rfogliat@andrew.cmu.edu',
+    packages=['scrapITRA'],
+    scripts=['bin/write_races.py'],
+    url='https://github.com/ricfog/ScrapITRA',
+    description='Scraping website of ITRA',
+    install_requires=[
+        "beautifulsoup4",
+    ],
+)
